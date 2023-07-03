cc8897f1f41617c5dde7a506550b0cadd5328cee
DoVietHoang1712 <hoang171220001@gmail.com>
2023-07-03 21:25:28 +0700

    Update eks version

--- terraform-modules/aws/eks/main.tf
+++ terraform-modules/aws/eks/main.tf
@@ -26,15 +26,15 @@ provider "kubernetes" {
 }
 
 resource "aws_kms_key" "eks" {
-  description = "EKS Secret Encryption Key"
+  description         = "EKS Secret Encryption Key"
   enable_key_rotation = var.cluster_kms_enable_rotation
-  tags        = var.tags
+  tags                = var.tags
 }
 
 module "kms_cloudwatch_log_group" {
-  source                  = "github.com/ManagedKube/kubernetes-ops.git//terraform-modules/aws/kms/cloudwatch_log_group?ref=v2.0.37"
-  log_group_name          = "/aws/eks/${var.cluster_name}/cluster"
-  tags                    = var.tags
+  source         = "github.com/ManagedKube/kubernetes-ops.git//terraform-modules/aws/kms/cloudwatch_log_group?ref=v2.0.37"
+  log_group_name = "/aws/eks/${var.cluster_name}/cluster"
+  tags           = var.tags
 }
 
 //EKS 1.23
@@ -49,7 +49,7 @@ https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-a
 resource "aws_eks_addon" "csi_driver" {
   cluster_name             = module.eks.cluster_id
   addon_name               = "aws-ebs-csi-driver"
-  addon_version            = "v1.11.4-eksbuild.1"
+  addon_version            = "v1.16.1-eksbuild.1"
   service_account_role_arn = aws_iam_role.eks_ebs_csi_driver.arn
 }
 
@@ -83,12 +83,12 @@ resource "aws_iam_role_policy_attachment" "amazon_ebs_csi_driver" {
 
 
 module "eks" {
-  source           = "terraform-aws-modules/eks/aws"
-  version          = "18.23.0"
-  cluster_name     = var.cluster_name
-  cluster_version  = var.cluster_version
-  enable_irsa      = var.enable_irsa
-  tags             = var.tags
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "19.15.0"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  enable_irsa     = var.enable_irsa
+  tags            = var.tags
 
   # vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
   vpc_id = var.vpc_id
@@ -101,16 +101,16 @@ module "eks" {
   cluster_endpoint_public_access       = var.cluster_endpoint_public_access
   cluster_endpoint_public_access_cidrs = var.cluster_endpoint_public_access_cidrs
 
-  cluster_endpoint_private_access                = var.cluster_endpoint_private_access
-  
+  cluster_endpoint_private_access = var.cluster_endpoint_private_access
+
   cluster_encryption_config = [{
     provider_key_arn = aws_kms_key.eks.arn
     resources        = ["secrets"]
   }]
 
-  cloudwatch_log_group_kms_key_id = module.kms_cloudwatch_log_group.kms_arn
+  cloudwatch_log_group_kms_key_id        = module.kms_cloudwatch_log_group.kms_arn
   cloudwatch_log_group_retention_in_days = var.cloudwatch_log_group_retention_in_days
-  cluster_enabled_log_types     = var.cluster_enabled_log_types
+  cluster_enabled_log_types              = var.cluster_enabled_log_types
 
   eks_managed_node_groups = var.eks_managed_node_groups
 
